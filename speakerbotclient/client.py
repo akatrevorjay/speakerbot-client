282601be30aac82b0f7520f8de3aba61e4c17b9d
Trevor Joynson (trevorj) <github@skywww.net>
2015-06-17 20:28:38 -0400

    Minor doc updates, return object where we should

--- speakerbotclient/client.py
+++ speakerbotclient/client.py
@@ -10,34 +10,53 @@ class SpeakerbotClient(object):
     """Speakerbot Client API"""
 
     def __init__(self, base_uri='http://speakerbot.local'):
+        # Hint: Set base_uri to the IP to avoid timely mDNS lookups.
         self.base_uri = base_uri
 
     def _absolutify_url(self, uri):
+        """
+        Helper to return an absolute URI. Yes, this exists.
+        :param uri: Relative URI to transform
+        :return: Absolute URI
+        """
         return '{}/{}'.format(self.base_uri, uri)
 
     def _get(self, uri):
+        """
+        Send a GET request, yo
+        :param uri: Relative URI to hit
+        :return: request object
+        """
         uri = self._absolutify_url(uri)
         return requests.get(uri)
 
     def _post(self, uri, data):
+        """
+        Send a POST request with delicious data, yo
+        :param uri: Relative URI to hit
+        :param data: Dictionary of POST data
+        :return: request object
+        """
         uri = self._absolutify_url(uri)
         return requests.post(uri, data=data)
 
     def _downvote(self, image='w5SlJ6q.gif'):
-        """Downvotes an image, yo
-        :param image: Image filename
-        :return:
         """
-        self._get('image/{}/downgoat'.format(image))
+        Downvotes an image, yo
+        :param image: Image filename
+        :return: request object
+        """
+        return self._get('image/{}/downgoat'.format(image))
 
     _downgoat = _downvote
 
     def _upvote(self, image='w5SlJ6q.gif'):
-        """Upvotes an image, yo
-        :param image: Image filename
-        :return:
         """
-        self._get('image/{}/upboat'.format(image))
+        Upvotes an image, yo
+        :param image: Image filename
+        :return: request object
+        """
+        return self._get('image/{}/upboat'.format(image))
 
     _upboat = _upvote
 
